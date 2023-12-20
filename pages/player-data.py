a834a68aeef428f7e6b00d7dbfeb8b350fd295cc
Niek Verdegaal <n.j.m.verdegaal@student.tue.nl>
2023-12-20 15:49:21 +0100

    HALLLO

--- pages/player-data.py
+++ pages/player-data.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 import re
 
 df_player_stats = pd.read_csv('../Data/FIFA World Cup 2022 Player Data/player_stats.csv', delimiter=',')
+df_scaled = pd.read_csv('df_scaled.csv')
 
 colors = {
     'background': '#0E1B2A',
@@ -25,4 +26,29 @@ layout = html.Div([
     html.Div([
             dcc.Dropdown(df_player_stats["player"].unique(), "Denzel Dumfries", id='demo-dropdown', searchable=True, style = {'color' : colors['dropdown']})
         ]),
+        html.Div([
+            dash_table.DataTable(
+       id='player-ratings',
+       columns=[{"name": i, "id": i} for i in df_scaled[['name', 'rating', 'match']].columns],
+        data=df_scaled[df_scaled["name"] == "Denzel Dumfries"][['name', 'rating', 'match']].to_dict('records'),
+        editable=True,
+    #     style_header={
+    #     'backgroundColor': 'rgb(30, 30, 30)',
+    #     'color': 'white'
+    # },
+    #     style_data={
+    #     'backgroundColor': 'rgb(50, 50, 50)',
+    #     'color': 'white'
+    # },
+   ),
     ])
+])
+
+@callback(
+    Output('player-ratings', 'data'),
+    [Input('demo-dropdown', 'value')]
+)
+
+def update_table(value):
+    filtered_data = df_scaled[df_scaled["name"] == value][['name', 'rating', 'match']].to_dict('records')
+    return filtered_data
