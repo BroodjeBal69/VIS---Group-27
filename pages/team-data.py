23bac6198b704ed22d5a5bf5743474bdf8b0f78c
Niek Verdegaal <n.j.m.verdegaal@student.tue.nl>
2024-01-30 11:36:24 +0100

    Koning dash heeft het gefixt

--- pages/team-data.py
+++ pages/team-data.py
@@ -17,6 +17,8 @@ df_players = df_players_team[['player', 'position', 'games', 'goals', 'assists']
 df_team_rating = pd.read_csv('df_rating_team.csv')
 
 df_team_rating_scaled = df_team_rating[df_team_rating['name'] == 'Argentina']
+team_lst = list(df_team_data["team"].unique())
+team_lst.insert(0, "~")
 
 colors = {
     'background': '#0E1B2A',
@@ -62,6 +64,10 @@ layout = html.Div([
         data=df_player_stats[df_player_stats["team"] == "Argentina"][['player', 'position', 'games', 'goals', 'assists']].to_dict('records'),
         editable=True
    ),
+   html.Div('Select team to compare(purple line):'),
+    html.Div([
+        dcc.Dropdown(team_lst, value="~", id='team-data-new-dropdown', searchable=True, style = {'color' : colors['dropdown']})
+    ]),
     ]),
     html.Div([
             dcc.Graph(
@@ -81,14 +87,15 @@ def update_table(value):
 
 @callback(
     Output('sub-graphs', 'figure'),
-    Input('demo-dropdown', 'value'), prevent_initial_call=True, allow_duplicates=True
+    Input('demo-dropdown', 'value'),
+     Input('team-data-new-dropdown', 'value'), prevent_initial_call=True, allow_duplicates=True, suppress_callback_exceptions=True
 )
-def update_graph(value):
+def update_graph(first_team, second_team):
 
-    filtered_data = df_player_stats[df_player_stats["team"] == value][['player', 'position', 'games', 'goals', 'assists']]
+    filtered_data = df_player_stats[df_player_stats["team"] == first_team][['player', 'position', 'games', 'goals', 'assists']]
     filtered_goals = filtered_data.sort_values('goals', ascending=False).head(5)
     filtered_assists = filtered_data.sort_values('assists', ascending=False).head(5)
-    df_team_rating_scaled = df_team_rating[df_team_rating['name'] == value]
+    df_team_rating_scaled = df_team_rating[df_team_rating['name'] == first_team]
     fig = make_subplots(rows=2, cols=2, specs=[[{}, {}],
             [{"colspan": 2}, None]], subplot_titles=('Topscorers', "Most assists"))
     fig.add_trace(row=1, col=1,
@@ -100,6 +107,9 @@ def update_graph(value):
     fig.add_trace(row=2, col=1,
         trace=go.Scatter(y=df_team_rating_scaled['rating_team'])
         )
+    if second_team != "~":
+        df_team_rating_scaled2 = df_team_rating[df_team_rating['name'] == second_team]
+        fig.add_trace(row=2, col=1, trace=go.Scatter(y=df_team_rating_scaled2['rating_team']))
 
     fig.update_yaxes(title_text="Goals", row=1, col=1)
     fig.update_yaxes(title_text="Assists", row=1, col=2)
